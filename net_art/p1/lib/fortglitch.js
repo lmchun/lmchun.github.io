dd442e0d4fafe2837f903f7b4e3024772eb065df
lmchun <chrischun@Chriss-MacBook-Pro.local>
2019-02-14 03:40:52 -0800

    asdfdgfhgjhk,

---
+++ net_art/p1/lib/fortglitch.js
@@ -0,0 +1,29 @@
+var img;
+
+function preload(){
+img=loadImage('no-button-backg.png');
+     soundFormats('mp4','ogg');
+    PresetMusic = loadSound('music.mp4');
+ bcreate=loadImage('creative-button.jpg');
+ bbattle=loadImage('battle-royale-button.jpg');
+}
+
+function setup() {
+  createCanvas(1601, 902);
+//  background (0);
+//  img = loadImage("fbr.jpg");
+//  image(img, 0, 0, width, height);
+    image(img, 0, 0,1601, 902);
+//    image(bbattle,100,500,246,59);
+//
+//    image(bcreate,300,500,181,70);
+// 
+} 
+
+
+function draw(){
+
+    
+    
+
+}
